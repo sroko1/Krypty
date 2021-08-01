80ad20c580d45601671386d85b97fbb9480f93b0
maniek <mpastuszka@gmail.com>
2021-08-01 10:23:20 +0200

    [init] - initial project commit

---
+++ src/main/java/com/example/cryptokrypto/CryptoKryptoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptokrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoKryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoKryptoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptokrypto/CryptoKryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptokrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoKryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
