7e02de0a42f0d59a1cc06ec46f0ff6563af2ee12
maniek <mpastuszka@gmail.com>
2021-08-22 15:04:46 +0200

    [change] - properties names customization

--- src/main/java/com/example/cryptokrypto/dto/WeatherDto.java
+++ src/main/java/com/example/cryptokrypto/dto/WeatherDto.java
@@ -1,5 +1,8 @@
 package com.example.cryptokrypto.dto;
 
-public record WeatherDto(Long id, String myCity,
-                         Long tempInCelsius) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record WeatherDto(Long id,
+                         @JsonProperty("moje_miasto") String myCity,
+                         @JsonProperty("temp_in_cs") Long tempInCelsius) {
 }
