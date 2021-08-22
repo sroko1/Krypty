09cd93ab2ab19b7cc04652a8b80d3d73b93f9707
maniek <mpastuszka@gmail.com>
2021-08-22 15:43:52 +0200

    [change] - http codes description + getWeatherById endpoint implemented

--- src/main/java/com/example/cryptokrypto/controller/WeatherController.java
+++ src/main/java/com/example/cryptokrypto/controller/WeatherController.java
@@ -4,6 +4,7 @@ import com.example.cryptokrypto.dto.WeatherDto;
 import com.example.cryptokrypto.service.WeatherService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,14 @@ public class WeatherController {
     public List<WeatherDto> getAll() {
         log.info("getAll");
         return weatherService.getAllWeathers();
+    }
+
+    // /weathers/1
+    // /weathers/99
+    @GetMapping("/weathers/{id}")
+    public WeatherDto getWeatherById(@PathVariable("id") Long id) {
+        log.info("getWeatherById: [{}]", id);
+        var result = weatherService.findWeatherById(id);
+        return result.orElse(null);
     }
 }
