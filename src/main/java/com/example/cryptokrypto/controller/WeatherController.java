25bef1a96f54174b48e3a266278c7d6b26f5f9df
maniek <mpastuszka@gmail.com>
2021-08-29 15:53:26 +0200

    [add] - update prototypes endpoint created

--- src/main/java/com/example/cryptokrypto/controller/WeatherController.java
+++ src/main/java/com/example/cryptokrypto/controller/WeatherController.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -81,4 +83,17 @@ public class WeatherController {
 //        }
         return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
+
+    // replace
+    @PutMapping("/weathers/{id}")
+    public void replaceWeatherForecast(@PathVariable("id") Long id, @RequestBody WeatherDto objectToReplace) {
+
+    }
+
+    // partial update
+    @PatchMapping("/weathers/{id}")
+    public void updateWeatherForecast(@PathVariable("id") Long id, @RequestBody WeatherDto updateDto) {
+
+    }
+
 }
