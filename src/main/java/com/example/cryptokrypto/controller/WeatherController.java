a5621bc19ce7cf427a3b553bd1d7444a6879f7fb
maniek <mpastuszka@gmail.com>
2021-10-03 21:44:05 +0200

    [change] - enabling CORS for every requests

--- src/main/java/com/example/cryptokrypto/controller/WeatherController.java
+++ src/main/java/com/example/cryptokrypto/controller/WeatherController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 @Slf4j
