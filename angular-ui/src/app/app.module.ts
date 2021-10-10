f02a1a7653fa481823c40d7836aa8a5ebb95247f
maniek <mpastuszka@gmail.com>
2021-10-10 11:38:24 +0200

    [change] - routing implemented

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -7,18 +7,35 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { GithubComponent } from './components/github/github.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'github', component: GithubComponent },
+  { path: 'pogoda', component: WeatherComponent },
+  { path: '', redirectTo: '/github', pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherComponent,
-    GithubComponent
+    GithubComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
